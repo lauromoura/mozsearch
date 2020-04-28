7b2cecb12978ec862fb22ce5e28507afcc085125
Diego Pino Garcia <dpino@igalia.com>
2020-04-28 08:49:45 +0000

    mkdirs.sh: mute output

--- scripts/mkdirs.sh
+++ scripts/mkdirs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x # Show commands
+# set -x # Show commands
 set -eu # Errors/undefined vars are fatal
 set -o pipefail # Check all commands in a pipeline
 
@@ -20,11 +20,11 @@ mkdir -p $INDEX_ROOT/analysis/__GENERATED__
 set +x # This part is annoyingly verbose, silence commands and use manual echo statement
 cat $INDEX_ROOT/repo-dirs | while IFS= read dir
 do
-    echo "Making {file,dir,analysis,description} dirs for $dir"
+    # echo "Making {file,dir,analysis,description} dirs for $dir"
     mkdir -p "$INDEX_ROOT/file/$dir"
     mkdir -p "$INDEX_ROOT/dir/$dir"
     mkdir -p "$INDEX_ROOT/analysis/$dir"
     mkdir -p "$INDEX_ROOT/description/$dir"
 done
-set -x
+# set -x
 mkdir -p $INDEX_ROOT/templates
